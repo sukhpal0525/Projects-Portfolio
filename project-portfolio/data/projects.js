6a219a3da1b30657aa886703ab1e2c6e07b13321
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-11-09 23:14:55 +0000

    Add example dynamic project data

---
+++ project-portfolio/data/projects.js
@@ -0,0 +1,4 @@
+export const projects = [
+    { title: 'Project 1', technologies: 'HTML, CSS, JavaScript, Bootstrap, Laravel' },
+    { title: 'Project 2', technologies: 'React, Node.js, Express' },
+];
