ccb73e738ed0c251ecf10a0d0cf63090e29f7bfe
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-11-12 23:03:29 +0000

    Refactor original project card HTML to new ProjectCard component

---
+++ project-portfolio/src/components/ProjectCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function ProjectCard({ title, technologies }) {
+    return (
+        <div className="col">
+            <div className="card bg-dark border-light h-100">
+                <img src="" className="card-img-top" style={{ backgroundColor: '#343a40', width: '100%', height: '250px' }} alt="Project" />
+                <div className="card-body text-center" style={{ padding: '10px', borderTop: '1px solid #fff' }}>
+                    <h5 className="card-title text-light" style={{ fontSize: '1rem' }}>{title}</h5>
+                    <p>{technologies}</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectCard;
