2cd40fb7cdf89640fbc1ae462df42fa25e955663
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-11-30 23:58:29 +0000

    Refactor ProjectCard component for conciseness

--- project-portfolio/src/components/ProjectCard.jsx
+++ project-portfolio/src/components/ProjectCard.jsx
@@ -1,16 +1,16 @@
-/* eslint-disable react/prop-types */
-function ProjectCard({ title, technologies }) {
+import ProjectCard from './components/ProjectCard';
+import { projects } from './data/projects';
+
+function App() {
     return (
-        <div className="col">
-            <div className="card bg-dark border-light h-100">
-                <img src="" className="card-img-top" style={{ backgroundColor: '#343a40', width: '100%', height: '250px' }} alt="Project" />
-                <div className="card-body text-center" style={{ padding: '10px', borderTop: '1px solid #fff' }}>
-                    <h5 className="card-title text-light" style={{ fontSize: '1rem' }}>{title}</h5>
-                    <p>{technologies}</p>
-                </div>
+        <div className="container">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 justify-content-center">
+                {projects.map((project, index) => (
+                    <ProjectCard key={index} title={project.title} technologies={project.technologies} />
+                ))}
             </div>
         </div>
     );
 }
 
-export default ProjectCard;
+export default App;
