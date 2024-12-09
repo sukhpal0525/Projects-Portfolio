591ac11ced9fe67a3482a42f15d56ba1caa25698
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2024-12-09 22:25:30 +0000

    Update projects.js

--- project-portfolio/data/projects.js
+++ project-portfolio/data/projects.js
@@ -1,8 +1,8 @@
-export const projects = [
-    { title: 'Project 1', technologies: 'HTML, CSS, JavaScript, Bootstrap, Laravel' },
-    { title: 'Project 2', technologies: 'React, Node.js, Express' },
-    { title: 'Project 3', technologies: 'Vue.js, Firebase' },
-    { title: 'Project 4', technologies: 'Angular, TypeScript' },
-    { title: 'Project 5', technologies: 'Django, Python' },
-    { title: 'Project 6', technologies: 'ASP.NET, C#' },
-];
+[
+    { "title": "Project 1", "technologies": "HTML, CSS, JavaScript, Bootstrap, Laravel" },
+    { "title": "Project 2", "technologies": "React, Node.js, Express" },
+    { "title": "Project 3", "technologies": "Vue.js, Firebase" },
+    { "title": "Project 4", "technologies": "Angular, TypeScript" },
+    { "title": "Project 5", "technologies": "Django, Python" },
+    { "title": "Project 6", "technologies": "ASP.NET, C#" }
+]
